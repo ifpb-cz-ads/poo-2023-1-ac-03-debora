97581b67f951182aeed17521f146fc26aede00a0
Débora Fernandes C <95572299+Deboraaluna@users.noreply.github.com>
2023-08-30 07:44:20 -0300

    Create BMExercicio02.java

---
+++ BMExercicio02.java
@@ -0,0 +1,17 @@
+class BMExercio02 {
+    public static void main(String[] args) {
+        int i = 1;
+        long j = 2;
+        float p = 20.0f;
+        double q = 30.0;
+        boolean b = true;
+        char c = 'k';
+
+        System.out.println("\nInteiro de 32 bits: " + i);
+        System.out.println("Inteiro de 64 bits: " + j);
+        System.out.println("Flutuante de 32 bits: " + p);
+        System.out.println("Flutuante de 64 bits: " + q);
+        System.out.println("Boolean Verdadeiro: " + b);
+        System.out.println("Caracter: " + c);
+    }
+}
