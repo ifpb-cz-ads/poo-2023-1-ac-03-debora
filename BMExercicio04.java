3e811d7984aa9e63a8aa5e4534b0bc17b90ed333
Débora Fernandes C <95572299+Deboraaluna@users.noreply.github.com>
2023-08-30 07:46:45 -0300

    Create BMExercicio04.java

---
+++ BMExercicio04.java
@@ -0,0 +1,8 @@
+public class BMExercicio04 {
+    public static void main(String[] args) {
+        long long1 = 100;
+        int int1 = 200;
+        int int2 = int1 + (int) long1;// tipos diferentes não fazem operaçoes.
+        System.out.println(int2);
+    }
+}
